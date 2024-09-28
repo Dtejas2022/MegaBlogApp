2fb959bba7f9562420d25787c122abee440933ac
Tejas Dotkar <106228821+Dtejas2022@users.noreply.github.com>
2024-09-28 07:14:17 +0000

    aythontication added

--- MegaBlogApp/src/appwrite/auth.js
+++ MegaBlogApp/src/appwrite/auth.js
@@ -2,13 +2,56 @@ import conf from "../conf/conf";
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
-    client = Client();
+    client = new Client();
     account;
 
     constructor(){
         this.client
-        .setEndpoint(conf.appWriteUrl) 
-        .setProject(conf.appWriteProjectId);  
+            .setEndpoint(conf.appWriteUrl) 
+            .setProject(conf.appWriteProjectId);  
+        this.account = new Account(this.client);
+
+    }
+    
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique() ,email, password, name);
+            if (userAccount) {
+                //call another method
+                return this.logIn({email, password});
+            } else {
+                return userAccount; 
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async logIn({email, password}){
+        try {
+           return await this.account.createEmailPasswordSession(email, password);
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentIUser(){
+        try {
+            return await this.account.get()
+        } catch (error) {
+            throw error;
+        }
+
+        return null;
+    }
+
+    async logout(){
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            throw error;
+        }
     }
 }
 
